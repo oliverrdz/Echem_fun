947a773c240c890b43179d0c40cd812c56df01c9
oliverrdz <oliver.rdz@gmail.com>
2021-03-17 22:26:07 -0500

    Added Perry and Denuault equation and plotting 3 curves on one fig

--- echem_fun.py
+++ echem_fun.py
@@ -12,6 +12,8 @@ i [A]
 import numpy as np
 
 F = 96485 # C/mol
+R = 8.3145 # J/molK
+T = 298 # K
 
 def ilim_disc(n=1, D=1e-5, C=1e-6, a=12.5e-4):
     ilim = 4*n*F*D*C*a
@@ -24,12 +26,29 @@ def shoup_Szabo(t, n=1, D=1e-5, C=1e-6, a=12.5e-4):
     return i
 
 def mahon_Oldham(t, n=1, D=1e-5, C=1e-6, a=12.5e-4):
+    # Written for a reduction
     s = D*t/a**2
     f1 = 1/np.sqrt(np.pi*s) + 1 + np.sqrt(s/(4*np.pi)) - 3*s/25 + 3*s**(3/2)/226
     f2 = 4/np.pi + 8/np.sqrt(s*np.pi**5) + 25/(2792*s**(3/2)) - 1/(3880*s**(5/2)) - 1/(4500*s**(7/2))
-    # Fancy if statement using boolean algebra:
+    # Fancy if statement using boolean operations:
     iNorm = (s<1.281)*f1 + (s>=1.281)*f2
     i = np.pi*n*F*D*C*a*iNorm
     return i
+
+def perry_Denuault(t, n=1, E0=0, E=-0.5, DO=1e-5, DR=1e-5, C=1e-6, a=12.5e-4, k0=1e-3, alpha=0.5):
+    '''
+    Sampled-Current Voltammetry at Microdisk Electrodes: Kinetic Information from Pseudo Steady State Voltammograms
+    Perry, Al Shandoudi, Denuault, Analytical Chemistry, 2014, 86, 9917-9923
+    doi: 10.1021/ac502645e
+    '''
+    fO = mahon_Oldham(t, n, DO, C, a)/(np.pi*n*F*DO*C*a)
+    fR = mahon_Oldham(t, n, DR, C, a)/(np.pi*n*F*DR*C*a)
+    Theta = 1 + (DO*fO/(DR*fR))*np.exp(n*F*(E-E0)/(R*T))
+    kappa = (k0*a/(DO*fO))*np.exp(-alpha*n*F*(E-E0)/(R*T))
+    i = (np.pi*n*F*DO*C*a*fO/Theta)/(1+(np.pi/(kappa*Theta))*((2*kappa*Theta+3*np.pi)/(4*kappa*Theta+3*np.pi**2)))
+    return i
+
+
+
 
 
--- plotting.py
+++ plotting.py
@@ -14,10 +14,21 @@ def plot(x, y, xlab, ylab, fig=1, mark='-'):
     format_plot()
     plt.show()
 
-def plot2(x1, y1, x2, y2, xlab, ylab, lab1, lab2, fig=1, mark1='-', mark2='-'):
+def plot2(x1, y1, x2, y2, xlab, ylab, lab1, lab2, fig=1, mark1='-', mark2='--'):
     plt.figure(fig)
     plt.plot(x1, y1, mark1, label=lab1)
     plt.plot(x2, y2, mark2, label=lab2)
+    plt.xlabel(xlab, fontsize=18)
+    plt.ylabel(ylab, fontsize=18)
+    plt.legend()
+    format_plot()
+    plt.show()
+
+def plot3(x1, y1, x2, y2, x3, y3, xlab, ylab, lab1, lab2, lab3, fig=1, mark1='-', mark2='--', mark3='-'):
+    plt.figure(fig)
+    plt.plot(x1, y1, mark1, label=lab1)
+    plt.plot(x2, y2, mark2, label=lab2)
+    plt.plot(x3, y3, mark3, label=lab3)
     plt.xlabel(xlab, fontsize=18)
     plt.ylabel(ylab, fontsize=18)
     plt.legend()
